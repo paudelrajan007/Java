8a372baf3aba099fe2dae29c33981efe6a881c9f
paudelrajan007 <rajan11paudel@gmail.com>
2025-10-10 10:30:47 +0530

    day 2

---
+++ ifelse.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class ifelse{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter int number between 1 to 100 : ");
+        int num=sc.nextInt();
+        if (num>0 && num<=50){
+            System.out.println("the num is between 1 to 50");
+            
+        }
+            else {
+                    System.out.println("the num is greater then 50 ");
+            }
+            }
+        }
+
+    
+    
